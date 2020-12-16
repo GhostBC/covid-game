7b42255bb36c070f5241d9f453109c6e0f7d3c0c
GhostBC <roddkevin@gmail.com>
2020-12-16 14:25:54 +0100

    player movement + debug scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,38 +1,73 @@
 ﻿using System.Collections;
+
+
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-  public float speed;
-
-    Rigidbody rb;
-    Vector3 moveDirection;
     Animator anim;
+    protected CharacterController characterController;
 
-    // Start is called before the first frame update
+    protected Vector3 movement;
+
+    public float _rotationSpeed = 180;
+
+    private Vector3 rotation;
+
     void Start()
     {
-       rb = GetComponent<Rigidbody>(); 
-       anim = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float mH = Input.GetAxis ("Horizontal");
-        float mV = Input.GetAxis ("Vertical");
-        rb.velocity = new Vector3 (mH * speed, rb.velocity.y, mV * speed) *-1;
 
-        if(Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0  ) {
+        if (characterController.isGrounded == true)
+        {
 
-           anim.SetBool("isWalking", true);
+            movement = new Vector3(
+               0, 0, Input.GetAxis("Vertical"));
+            movement = transform.TransformDirection(movement);
+            movement *= 5.0f;
 
-        } else {
-               anim.SetBool("isWalking", false);
+            if (movement.x != 0 || movement.y != 0)
+            {
+                anim.SetBool("isWalking", true);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+            }
+
+
+
+            if (Input.GetKey(KeyCode.Space) == true)
+            {
+                movement.y = 10.0f;
+                anim.SetBool("isWalking", false);
+            }
         }
 
+        movement.y -= 20.0f * Time.deltaTime;
 
+        characterController.Move(movement * Time.deltaTime);
+
+
+
+    }
+
+    void LateUpdate()
+    {
+        this.rotation = new Vector3(0, Input.GetAxis("Horizontal") * _rotationSpeed * Time.fixedDeltaTime, 0);
+
+        this.transform.Rotate(this.rotation);
     }
 }
