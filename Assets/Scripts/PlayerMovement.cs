b52868a4f8541a68a94c1659bd3a7de227c59532
Andreas <andreas>
2020-12-16 12:48:20 +0100

    all

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  public float speed;
+
+    Rigidbody rb;
+    Vector3 moveDirection;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>(); 
+       anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float mH = Input.GetAxis ("Horizontal");
+        float mV = Input.GetAxis ("Vertical");
+        rb.velocity = new Vector3 (mH * speed, rb.velocity.y, mV * speed) *-1;
+
+        if(Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0  ) {
+
+           anim.SetBool("isWalking", true);
+
+        } else {
+               anim.SetBool("isWalking", false);
+        }
+
+
+    }
+}
