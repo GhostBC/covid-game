60b046800c455f66c1aef9ff34436212d5fb1d3f
GhostBC <ghost@bc.com>
2020-12-19 22:04:28 +0100

    Cleanup

--- Assets/Selbst erstellt/Scripts/PlayerMovement.cs
+++ Assets/Selbst erstellt/Scripts/PlayerMovement.cs
@@ -8,137 +8,115 @@ using UnityEngine;
 
 public class PlayerMovement: MonoBehaviour {
 
-	Animator anim;
-	protected CharacterController characterController;
-	private float speed = 5;
-	public float gravity = 9.81f;
-	private float verticalSpeed = 0;
+  Animator anim;
+  protected CharacterController characterController;
+  private float speed = 5;
+  public float gravity = 9.81f;
+  private float verticalSpeed = 0;
 
-	public Transform cameraHolder;
-	private float mouseSensitivity = 2f;
-	private float upLimit = -50;
-	private float downLimit = 50;
-	public AudioClip jumpSound;
-	public AudioClip walkSound;
-	private AudioSource  audioSource;
+  public Transform cameraHolder;
+  private float mouseSensitivity = 2f;
+  private float upLimit = -50;
+  private float downLimit = 50;
+  public AudioClip jumpSound;
+  public AudioClip walkSound;
+  private AudioSource audioSource;
 
-	
-    private float verticalRotation;
-    private float horizontalRotation;
+  private float verticalRotation;
+  private float horizontalRotation;
 
+  void Start() {
 
-	void Start() {
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+    verticalRotation = transform.localEulerAngles.x;
+    horizontalRotation = transform.eulerAngles.y;
 
-		    Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-		verticalRotation = transform.localEulerAngles.x;
-        horizontalRotation = transform.eulerAngles.y;
+    characterController = GetComponent < CharacterController > ();
+    anim = GetComponent < Animator > ();
+    audioSource = GetComponent < AudioSource > ();
 
-		characterController = GetComponent < CharacterController > ();
-		anim = GetComponent < Animator > ();
-		 audioSource = GetComponent<AudioSource>();
-	
-	}
+  }
 
-	// Update is called once per frame
-	void Update() {
+  // Update is called once per frame
+  void Update() {
 
+    move();
 
-		look();
-		move();
-	
+  }
 
-	}
+  private void move() {
 
-	private void look() {
-		float _mouseVertical = -Input.GetAxis("Mouse Y");
-        float _mouseHorizontal = Input.GetAxis("Mouse X");
+    float _mouseVertical = -Input.GetAxis("Mouse Y");
+    float _mouseHorizontal = Input.GetAxis("Mouse X");
 
-        verticalRotation += _mouseVertical * (mouseSensitivity * 200)  * Time.deltaTime;
-        horizontalRotation += _mouseHorizontal *  (mouseSensitivity * 200) * Time.deltaTime;
+    verticalRotation += _mouseVertical * (mouseSensitivity * 200) * Time.deltaTime;
+    horizontalRotation += _mouseHorizontal * (mouseSensitivity * 200) * Time.deltaTime;
 
-        verticalRotation = Mathf.Clamp(verticalRotation, upLimit, downLimit);
+    verticalRotation = Mathf.Clamp(verticalRotation, upLimit, downLimit);
 
-        cameraHolder.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
-        transform.rotation = Quaternion.Euler(0f, horizontalRotation, 0f);
+    cameraHolder.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    transform.rotation = Quaternion.Euler(0f, horizontalRotation, 0f);
 
-		
-	}
+    float verticalMove = Input.GetAxis("Vertical");
 
+    Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
+    Vector3 move = transform.forward * verticalMove;
+    if (verticalMove < 0) {
+      this.speed = 4;
+    } else {
+      this.speed = 5;
 
-	private void move() {
-
-	
-		float verticalMove = Input.GetAxis("Vertical");
-
-	
-		Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
-		Vector3 move = transform.forward * verticalMove ;
-		if (verticalMove < 0) {
-			this.speed = 3;
-
-		} else {
-		
-				this.speed = 5;
-		
-			
-		}
-
-		
+    }
+	anim.SetFloat("speed", this.speed);
+    characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
 
 
-		if (Input.GetKey(KeyCode.Space) == true && anim.GetBool("isJumping") == false) {
-			verticalSpeed = 11;
+    if (verticalMove > 0) {
 
-			anim.SetBool("isJumping", true);
-			audioSource.Stop();
-			 audioSource.PlayOneShot(jumpSound, 0.1F);
+      anim.SetBool("isWalking", true);
+      if (!audioSource.isPlaying && characterController.isGrounded) {
+        audioSource.PlayOneShot(walkSound, 0.3F);
+      }
+      anim.SetBool("isWalkingBackwards", false);
+ 
+    } else {
 
-		}
+      if (verticalMove < 0 && characterController.isGrounded) {
+        anim.SetBool("isWalkingBackwards", true);
+      } else {
+        anim.SetBool("isWalkingBackwards", false);
+      }
+      anim.SetBool("isWalking", false);
 
-			if (characterController.isGrounded) {
-			if (anim.GetBool("isJumping")) {
-				anim.SetBool("isJumping", false);
-				anim.SetTrigger("exitJump");
-			
-			}
-			 verticalSpeed =  0;
+    }
 
-		} else {
-			verticalSpeed -= gravity  *  Time.deltaTime;
-		}
+    if (Input.GetKey(KeyCode.Space) == true && anim.GetBool("isJumping") == false) {
+      verticalSpeed = 11;
+      anim.SetBool("isJumping", true);
+      audioSource.Stop();
+      audioSource.PlayOneShot(jumpSound, 0.1F);
+
+    }
 
 
-		if(verticalMove > 0 ) {
-			  
-	 	 anim.SetBool("isWalking",true);
-		   if (!audioSource.isPlaying && characterController.isGrounded)
-        {
-		   audioSource.PlayOneShot(walkSound, 0.3F);
-		}
-		anim.SetBool("isWalkingBackwards",false);
-		  if(!Input.GetMouseButton(0)) {
-			  //Reset Camera behind the Player
-			  	Quaternion resetRotation = Quaternion.Euler(transform.rotation.eulerAngles);
-	 			 cameraHolder.rotation = Quaternion.Lerp(cameraHolder.rotation, resetRotation, Time.deltaTime );
-		  }
-		} else {
 
-			if(verticalMove < 0 && characterController.isGrounded) {
-				anim.SetBool("isWalkingBackwards",true);
-			} else {
-				anim.SetBool("isWalkingBackwards",false);
-			}
-			anim.SetBool("isWalking",false);
-			
-		}
+    if (characterController.isGrounded) {
+      if (anim.GetBool("isJumping")) {
+        anim.SetBool("isJumping", false);
+        anim.SetTrigger("exitJump");
 
-				anim.SetFloat("speed", this.speed);
-		characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
+      }
+     // verticalSpeed = gravity * Time.deltaTime;
 
-		
+    } else {
+     
+      verticalSpeed -= gravity * Time.deltaTime;
+    }
 
-	}
 
+
+
+  }
 
 }
